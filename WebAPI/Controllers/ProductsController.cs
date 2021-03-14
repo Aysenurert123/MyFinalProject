0932f673d645d64b8f2a4a05a6f954ad86c8a243
Aysenurert123 <erturkaysenur16@gmail.com>
2021-03-14 22:28:50 +0300

    refactor

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -53,6 +53,18 @@ namespace WebAPI.Controllers
             }
 
             return BadRequest(result);
+        } 
+
+        [HttpGet("getbycategory")]
+        public IActionResult GetByCategory(int categoryId)
+        {
+            var result = _productService.GetAllByCategoryId(categoryId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
         }
 
         [HttpPost("add")]
